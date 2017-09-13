dc124b8c591b90a84c356b4715cadf339f66d6fd
vicboma1 <victorbolinchesmarin@gmail.com>
2017-09-13 13:09:35 +0200

    spy example refactors

---
+++ src/test/java/spy/SpyTest.java
@@ -0,0 +1,49 @@
+package spy;
+
+import api.CUT;
+import api.ComponentExtension;
+import dummy.ComponentDummyExtensionImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import src.CUTImpl;
+
+public class SpyTest {
+
+    private ComponentExtension ce;
+
+    @Before
+    public void setUp() throws Exception {
+        ce = new ComponentDummyExtensionImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ce = null;
+    }
+
+    @Test
+    public void addSpy() throws Exception {
+
+        final CUT cutSpy = Mockito.spy(new CUTImpl(ce));
+        cutSpy.doBusiness("3", null);
+
+        Mockito.verify(cutSpy).doBusiness("3", null);
+        //Mockito.verify(ce).add(3);  ->> Error! ce is not spy
+
+    }
+
+    @Test
+    public void addSpyCe() throws Exception {
+
+        final ComponentExtension ceSpy =  Mockito.spy(ce);
+        final CUT cutSpy = Mockito.spy(new CUTImpl(ceSpy));
+        cutSpy.doBusiness("3", null);
+
+        Mockito.verify(cutSpy).doBusiness("3", null);
+        Mockito.verify(ceSpy).add(3);
+    }
+}
