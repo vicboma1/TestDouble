c6075b223e1df916d3959e9e2bdf32ab7c392df3
vicboma1 <victorbolinchesmarin@gmail.com>
2017-09-13 13:44:54 +0200

    mock example refactors

---
+++ src/test/java/mock/MockTest.java
@@ -0,0 +1,47 @@
+package mock;
+
+import api.CUT;
+import api.ComponentExtension;
+import dummy.ComponentDummyExtensionImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import src.CUTImpl;
+
+import static org.mockito.Matchers.anyInt;
+
+public class MockTest {
+    private ComponentExtension ce;
+    private CUT cut;
+
+    @Before
+    public void setUp() throws Exception {
+        ce = Mockito.mock(ComponentExtension.class);
+        cut = new CUTImpl(ce);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ce = null;
+        cut = null;
+    }
+
+    @Test
+    public void add() throws Exception {
+
+        Mockito.when(ce.add(Integer.MIN_VALUE)).thenReturn(Integer.MIN_VALUE + 1);
+        Mockito.when(ce.add(3)).thenReturn(3);
+        Mockito.when(ce.add(123)).thenReturn(124);
+
+        Assert.assertEquals("MIN_VALUE add(Integer.MIN_VALUE) != Integer.MIN_VALUE + 1", java.util.Optional.of(Integer.MIN_VALUE + 1), java.util.Optional.of(ce.add(Integer.MIN_VALUE)));
+        Assert.assertNotEquals("4 != add(3)", "4", cut.doBusiness(3, 1));
+        Assert.assertEquals("add(123) != 124", "124", cut.doBusiness(123, 1));
+
+        Mockito.verify(ce).add(Matchers.eq(Integer.MIN_VALUE));
+        Mockito.verify(ce, Mockito.times(3)).add(anyInt());
+    }
+}
